cb23dd787c991256c0118cda522338654441ca3c
Simon Donike <accounts@donike.net>
2025-10-29 10:49:32 +0000

    discr trest

--- opensr_srgan/tests/test_models/test_discriminators.py
+++ opensr_srgan/tests/test_models/test_discriminators.py
@@ -29,27 +29,3 @@ def test_discriminator_can_be_instantiated(discriminator_cls, kwargs):
     instance = discriminator_cls(**kwargs)
     assert isinstance(instance, nn.Module)
 
-
-def test_esrgan_discriminator_warns_about_n_blocks_override(capsys):
-    """SRGAN model should inform users when ESRGAN ignores discriminator n_blocks."""
-
-    config = OmegaConf.create(
-        {
-            "Model": {"in_bands": 3},
-            "Generator": {"model_type": "srresnet", "scaling_factor": 4},
-            "Discriminator": {"model_type": "esrgan", "n_blocks": 5},
-        }
-    )
-
-    model = SRGAN_model.__new__(SRGAN_model)
-    model.config = config
-    model.mode = "train"
-    model.generator = None
-    model.discriminator = None
-
-    model.get_models("train")
-
-    captured = capsys.readouterr()
-    assert (
-        "[Discriminator:esrgan] Ignoring unsupported configuration options: n_blocks." in captured.out
-    )
